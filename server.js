a0316ec4c47aba597d34cb10af0feaa71ca20cfa
Igor Napierala <igor.napierala@cognifide.com>
2021-02-03 09:07:10 +0100

    simple api and shortcuts for writing response

--- server.js
+++ server.js
@@ -2,10 +2,8 @@ const http = require("http");
 const products = require("./data/products");
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
-  res.write("<h1>Yo</h1>");
-  res.end();
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(JSON.stringify(products));
 });
 
 const PORT = process.env.PORT || 5000;
