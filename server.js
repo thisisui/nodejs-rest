e8f3ea5b75893ebde6c0341c981dd80d4399fcda
Igor Napierala <igor.napierala@cognifide.com>
2021-02-03 09:56:40 +0100

    first api products url check

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ const http = require("http");
 const products = require("./data/products");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(JSON.stringify(products));
+  if (req.url === "/api/products") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(products));
+  } else {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "Route Not Found" }));
+  }
 });
 
 const PORT = process.env.PORT || 5000;
