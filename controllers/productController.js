e219d345d296e22b36e198994841a100e220928e
Igor Napierala <igor.napierala@cognifide.com>
2021-02-03 23:55:55 +0100

    added get single product

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,7 @@
 const Product = require("../models/productModel");
 
+// @desc    Gets all Products
+// @route   GET /api/products
 async function getProducts(req, res) {
   try {
     const products = await Product.findAll();
@@ -11,6 +13,25 @@ async function getProducts(req, res) {
   }
 }
 
+// @desc    Gets Single Products
+// @route   GET /api/products/:id
+async function getProduct(req, res) {
+  try {
+    const product = await Product.findById(id);
+
+    if (!product) {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "Product Not Found" }));
+    } else {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(product));
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 module.exports = {
   getProducts,
+  getProduct,
 };
